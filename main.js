38b06be144ca94f0e9b000add37bce4316b0ff55
OrlandoJaimesPerico <orjapejea@gmail.com>
2021-03-26 21:24:17 -0500

    Clase 26-03-2021

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+// Variables y constantes
+
+let nombre;
+let apellido;
+const edad = 34;
+nombre = "Orlando";
+apellido = "Jaimes Perico";
+console.log(nombre, apellido, edad);
+
+
+// Tipos de datos
+let Nombre = `Orlando Jaimes Perico`; //String
+let Edad = 34; //number - intenger - int;
+let altura = 1.78; //Double - float
+let profesor = true; // Boleano
+let informacion = [Nombre, Edad, altura, profesor]; //Array
+console.log(informacion);
+// console.log(informacion[2]);
+let baso = {
+	material : "porcelan",
+	diametro :  60,
+	lleno : false
+}; // Object
+console.log(baso);
+// console.log(baso.diametro)
+
+
+
+
+
